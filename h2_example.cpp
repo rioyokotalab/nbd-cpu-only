f85851cbe3c130fb1429e5cfee79e6f485175ef3
Qianxiang Ma <931409754@qq.com>
2021-06-28 18:45:12 +0900

    timer

--- h2_example.cpp
+++ h2_example.cpp
@@ -4,6 +4,7 @@
 #include "aca.h"
 #include "h2mv.h"
 #include "test_util.h"
+#include "timer.h"
 
 #include <cstdio>
 #include <random>
@@ -29,33 +30,43 @@ int main(int argc, char* argv[]) {
   Cells c1 = buildTree(b1, leaf, dim);
   Cells c2 = buildTree(b2, leaf, dim);
 
+  start("build H");
   auto fun = dim == 2 ? l2d() : l3d();
   Matrices d, bi, bj;
   traverse(fun, c1, c2, dim, d, theta, rank);
+  stop("build H");
 
   {
+    start("verify H");
     Matrix a_ref(m, n, m), a_rebuilt(m, n, m);
     convertHmat2Dense(c1, c2, d, a_rebuilt, a_rebuilt.LDA);
     P2Pnear(fun, &c1[0], &c2[0], dim, a_ref);
     printf("H-mat compress err %e\n", rel2err(&a_rebuilt[0], &a_ref[0], m, n, m, m));
+    stop("verify H");
   }
 
+  start("build H2");
   traverse_i(c1, c2, d, bi, p);
   traverse_j(c1, c2, d, bj, p);
   shared_epilogue(d);
+  stop("build H2");
 
   {
+    start("verify H2");
     Matrix a_ref(m, n, m), a_rebuilt(m, n, m);
     convertH2mat2Dense(c1, c2, bi, bj, d, a_rebuilt, a_rebuilt.LDA);
     P2Pnear(fun, &c1[0], &c2[0], dim, a_ref);
     printf("H2-mat compress err %e\n", rel2err(&a_rebuilt[0], &a_ref[0], m, n, m, m));
+    stop("verify H2");
   }
 
 
   std::vector<double> x(n), b(m);
   vecRandom(&x[0], n, 1, 0, 1);
 
+  start("H2-vec");
   h2mv_complete(c1, c2, bi, bj, d, &x[0], &b[0]);
+  stop("H2-vec");
 
   std::vector<double> b_ref(m);
 
