698141177aff596c04ab5dcdcd3859ebdc532398
Qianxiang Ma <931409754@qq.com>
2022-04-13 03:17:08 +0900

    update readme

--- install_deps_apt.sh
+++ install_deps_apt.sh
@@ -2,7 +2,6 @@
 
 apt-get update
 apt-get -y install wget gnupg tzdata
-cd /tmp
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 rm GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
@@ -10,4 +9,3 @@ echo "deb https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.li
 apt-get update
 apt-get -y install mpich intel-basekit intel-hpckit
 . /opt/intel/oneapi/setvars.sh
-make
