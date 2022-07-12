3308bf55b782d14d2ce68df8d8432978ef5fa310
Qianxiang Ma <qianxiang.ma@case.edu>
2022-07-12 13:50:27 +0900

    updates

--- umv.cxx
+++ umv.cxx
@@ -154,10 +154,7 @@ void nextNode(Node& Anext, const CSC& rels_up, const Node& Aprev, const int64_t*
   const Matrix* Slow = Aprev.S_oo.data();
 
   int64_t plevel = nlevel + 1;
-  int64_t nloc = 0;
-  int64_t nend = (int64_t)1 << nlevel;
-  int64_t ploc = 0;
-  int64_t pend = (int64_t)1 << plevel;
+  int64_t nloc = 0, nend = 0, ploc = 0, pend = 0;
   selfLocalRange(&nloc, &nend, nlevel);
   selfLocalRange(&ploc, &pend, plevel);
 
@@ -284,9 +281,8 @@ void factorSpDense(SpDense& sp) {
 }
 
 void basisXoc(char fwbk, RightHandSides& vx, const Base& basis, int64_t level) {
-  int64_t len = 0, lbegin = 0, lend = 0;
+  int64_t lbegin = 0, lend = 0;
   selfLocalRange(&lbegin, &lend, level);
-  contentLength(&len, level);
 
   if (fwbk == 'F' || fwbk == 'f')
     for (int64_t i = lbegin; i < lend; i++) {
@@ -387,49 +383,30 @@ void svAocBk(Matrix* Xc, const Matrix* Xo, const Matrix* A_oc, const CSC& rels, 
 }
 
 void permuteAndMerge(char fwbk, Matrix* px, Matrix* nx, const int64_t* lchild, int64_t nlevel) {
-  int64_t plevel = nlevel + 1;
-  int64_t nloc = 0;
-  int64_t nend = (int64_t)1 << nlevel;
-  int64_t ploc = 0;
-  int64_t pend = (int64_t)1 << plevel;
+  int64_t nloc = 0, nend = 0;
   selfLocalRange(&nloc, &nend, nlevel);
-  selfLocalRange(&ploc, &pend, plevel);
   int64_t nboxes = nend - nloc;
 
-  if (fwbk == 'F' || fwbk == 'f') {
+  if (fwbk == 'F' || fwbk == 'f')
     for (int64_t i = 0; i < nboxes; i++) {
       int64_t c0 = lchild[i + nloc];
       int64_t c1 = c0 + 1;
       Matrix& x0 = nx[i + nloc];
-
-      if (c0 >= ploc && c0 < pend) {
-        const Matrix& x1 = px[c0];
-        cpyMatToMat(x1.M, x0.N, &x1, &x0, 0, 0, 0, 0);
-      }
-
-      if (c1 >= ploc && c1 < pend) {
-        const Matrix& x2 = px[c1];
-        cpyMatToMat(x2.M, x0.N, &x2, &x0, 0, 0, x0.M - x2.M, 0);
-      }
+      const Matrix& x1 = px[c0];
+      cpyMatToMat(x1.M, x0.N, &x1, &x0, 0, 0, 0, 0);
+      const Matrix& x2 = px[c1];
+      cpyMatToMat(x2.M, x0.N, &x2, &x0, 0, 0, x0.M - x2.M, 0);
     }
-  }
-  else if (fwbk == 'B' || fwbk == 'b') {
+  else if (fwbk == 'B' || fwbk == 'b')
     for (int64_t i = 0; i < nboxes; i++) {
       int64_t c0 = lchild[i + nloc];
       int64_t c1 = c0 + 1;
       const Matrix& x0 = nx[i + nloc];
-
-      if (c0 >= ploc && c0 < pend) {
-        Matrix& x1 = px[c0];
-        cpyMatToMat(x1.M, x0.N, &x0, &x1, 0, 0, 0, 0);
-      }
-
-      if (c1 >= ploc && c1 < pend) {
-        Matrix& x2 = px[c1];
-        cpyMatToMat(x2.M, x0.N, &x0, &x2, x0.M - x2.M, 0, 0, 0);
-      }
+      Matrix& x1 = px[c0];
+      cpyMatToMat(x1.M, x0.N, &x0, &x1, 0, 0, 0, 0);
+      Matrix& x2 = px[c1];
+      cpyMatToMat(x2.M, x0.N, &x0, &x2, x0.M - x2.M, 0, 0, 0);
     }
-  }
 }
 
 void allocRightHandSides(RightHandSides st[], const Base base[], int64_t levels) {
@@ -499,12 +476,8 @@ void solveA(RightHandSides st[], const Node A[], const Base B[], const CSC rels[
     basisXoc('F', st[i], B[i], i);
     svAccFw(st[i].Xc.data(), A[i].A_cc.data(), rels[i], i);
     svAocFw(st[i].Xo.data(), st[i].Xc.data(), A[i].A_oc.data(), rels[i], i);
+    DistributeMatricesList(st[i].Xo.data(), i);
     permuteAndMerge('F', st[i].Xo.data(), st[i - 1].X.data(), B[i - 1].Lchild, i - 1);
-
-    int comm_needed;
-    butterflyComm(&comm_needed, i);
-    if (comm_needed)
-      butterflySumA(st[i - 1].X.data(), st[i - 1].Xlen, i);
   }
   mat_solve('A', &st[0].X[0], &A[0].A[0]);
   
