f612b9a4749058bf8352362a016fd4a59f73d78e
wawando <muhammad.ridwan.ab@gmail.com>
2023-05-13 14:00:20 +0900

    Remove unused vars

--- dsyev.cxx
+++ dsyev.cxx
@@ -25,7 +25,7 @@
 
 int main(int argc, char* argv[]) {
   MPI_Init(&argc, &argv);
-  int mpi_rank, mpi_size, mpi_grid[2] = {0};
+  int mpi_rank, mpi_size;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
   if (mpi_size > 1) {
